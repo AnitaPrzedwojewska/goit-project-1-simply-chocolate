b179e5bc4aae99452c99ec1d06b5d76c8c23b788
KrolLew23 <145460261+KrolLew23@users.noreply.github.com>
2023-11-19 17:48:34 +0000

    Deploying to gh-pages from @ AnitaPrzedwojewska/project-1-simply-chocolate@a7609c7c1b2bb647c56924cf57c6eda454136835 🚀

---
+++ index.23600b95.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-review-open]"),closeModalBtn:document.querySelector("[data-modal-review-close]"),modal:document.querySelector("[data-modal-review]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.23600b95.js.map
---
+++ index.9028cd1e.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-explore-open]"),closeModalBtn:document.querySelector("[data-modal-explore-close]"),modal:document.querySelector("[data-modal-explore]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.9028cd1e.js.map
