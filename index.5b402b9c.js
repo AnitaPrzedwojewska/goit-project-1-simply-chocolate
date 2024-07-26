e3bc3fbccff010700e49b3216ac104c37bda3860
AnitaPrzedwojewska <144385209+AnitaPrzedwojewska@users.noreply.github.com>
2024-07-26 19:43:51 +0000

    Deploying to gh-pages from @ AnitaPrzedwojewska/project-1-simply-chocolate@2861c7e1c7b1201af04a394473703134d6ca0bf1 🚀

---
+++ index.5b402b9c.js
@@ -0,0 +1,2 @@
+new Swiper(".rev-slide-content",{direction:"horizontal",loop:!1,breakpoints:{slidesPerView:1,768:{slidesPerView:2,spaceBetween:16},1200:{slidesPerView:3,spaceBetween:28}},pagination:{el:".rev-slide-pagination",type:"bullets"}});
+//# sourceMappingURL=index.5b402b9c.js.map
